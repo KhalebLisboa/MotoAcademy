234768eb6eba7a9311222c241598bf52bf058cf7
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2023-04-05 19:23:24 -0400

    add: create login activity

--- app/src/main/java/com/project/lab02/MainActivity.java
+++ app/src/main/java/com/project/lab02/MainActivity.java
@@ -3,12 +3,29 @@ package com.project.lab02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnLogin;
+    TextView txtSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        btnLogin = findViewById(R.id.btn_login);
+        txtSignIn = findViewById(R.id.txt_create_account);
+
+        btnLogin.setOnClickListener(this::onClickGeneric);
+        txtSignIn.setOnClickListener(this::onClickGeneric);
+    }
+
+    void onClickGeneric(View view){
+        Snackbar.make(view, "Realizar ação", Snackbar.LENGTH_LONG).show();
     }
 }
