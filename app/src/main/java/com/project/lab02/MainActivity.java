23ea22ba4844959e87416788b3339f9b5656f6df
Khaleb Lisboa <khaleb.lisboa@gmail.com>
2023-04-11 12:53:17 -0400

    add: second activity

--- app/src/main/java/com/project/lab02/MainActivity.java
+++ app/src/main/java/com/project/lab02/MainActivity.java
@@ -1,8 +1,13 @@
 package com.project.lab02;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -25,7 +30,26 @@ public class MainActivity extends AppCompatActivity {
         txtSignIn.setOnClickListener(this::onClickGeneric);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.config:
+                Intent secondActivity = new Intent(this, SecondActivity.class);
+                startActivity(secondActivity);
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     void onClickGeneric(View view){
+        Log.e("Info", "Apertou o botão");
         Snackbar.make(view, "Realizar ação", Snackbar.LENGTH_LONG).show();
     }
 }
